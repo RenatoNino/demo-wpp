836aec6363e8ce2cf88b5637c0ac695c738be500
RenatoNino <jorgenino07@gmail.com>
2023-08-23 17:28:45 -0500

    feat: api to send messages

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 // Supports ES6
+const express = require('express');
+const app = express();
+const port = 3005; // Puedes cambiar el puerto si es necesario
+app.use(express.json()); // Middleware para parsear JSON en las solicitudes
+
 // import { create, Whatsapp } from 'venom-bot';
 const venom = require('venom-bot');
-
+var client = null;
 venom
   .create({
-    session: 'session-name' //name of session
+    session: 'session-name'
   })
-  .then((client) => start(client))
-  /* .catch((erro) => {
-    console.log(erro);
-  }) */;
+  .then((cl) => {
+    client = cl;
+  });
 
-function start(client) {
-    setInterval(() => {
-        const destinationNumber = '51920648575@c.us'; // Agrega @c.us al final del número
-        const message = '¡Este es un mensaje enviado automáticamente cada 10 segundos!';
+// function start(client) {
+//     setInterval(() => {
+//         const destinationNumber = '51920648575@c.us';
+//         const message = '¡Este es un mensaje enviado automáticamente cada 10 segundos!';
         
-        // Enviar el mensaje
-        client.sendText(destinationNumber, message)
-          .then((result) => {
-            console.log('Mensaje enviado: ', result); // Devuelve el objeto de éxito
-          })
-          .catch((error) => {
-            console.error('Error al enviar mensaje: ', error); // Devuelve el objeto de error
-          });
-      }, 10000); // Intervalo de 10000 milisegundos (10 segundos)
+//         client.sendText(destinationNumber, message)
+//           .then((result) => {
+//             console.log('Mensaje enviado: ', result);
+//           })
+//           .catch((error) => {
+//             console.error('Error al enviar mensaje: ', error);
+//           });
+//       }, 10000);
 
 //   client.onMessage((message) => {
 //     console.log(message);
@@ -37,7 +40,32 @@ function start(client) {
 //         // .catch((erro) => {
 //         //   console.error('Error when sending: ', erro); //return object error
 //         // });
-//         client.sendText(destinationNumber, 'Mensaje enviado al número 51920648575');
+//       client.sendText(destinationNumber, 'Mensaje enviado al número 51920648575');
 //     }
 //   });
-}
+// }
+
+
+app.post('/send-message', (req, res) => {
+  if(!client) {
+    res.status(500).send('Aún no se ha realizado la conexión a whatsapp.');
+  } else {
+    const destinationNumber = req.body.number;
+    const message = req.body.message;
+
+    client.sendText(destinationNumber + '@c.us', message)
+      .then((result) => {
+        console.log('Mensaje enviado: ', result);
+        res.status(200).send('Mensaje enviado con éxito');
+      })
+      .catch((error) => {
+        console.error('Error al enviar mensaje: ', error);
+        res.status(500).send('Error al enviar mensaje');
+    });
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
