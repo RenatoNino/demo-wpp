3e517b454ea901eafd52485a63ac28c56e73c25e
RenatoNino <jorgenino07@gmail.com>
2023-08-24 19:11:32 -0500

    feat: reboot enabled

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // Supports ES6
 const express = require('express');
 const app = express();
-const port = 3005; // Puedes cambiar el puerto si es necesario
-app.use(express.json()); // Middleware para parsear JSON en las solicitudes
+const port = 3005;
+app.use(express.json());
 
-// import { create, Whatsapp } from 'venom-bot';
 const venom = require('venom-bot');
 let client = null;
 let isSessionOpen = false;
@@ -18,23 +17,14 @@ venom
     //   // asciiQR contiene el código QR en formato ASCII
     //   console.log('Escanea este código QR:', asciiQR);
     // },
-    statusFind: (statusGet, session, info) => {
-      console.log("Cambio en la sesión")
-      console.log(statusGet)
-    }
+    // statusFind: (statusGet, session, info) => {
+    //   console.log("Cambio en la sesión")
+    //   console.log(statusGet)
+    // }
   })
-  .then((cl) => {
+  .then(async (cl) => {
     client = cl;
     isSessionOpen = true;
-
-    cl.onStateChange((state) => {
-      console.log('Cambió el estado de la sesión:');
-      console.log(state);
-      if (state === 'CONFLICT' || state === 'UNPAIRED' || state === 'UNLAUNCHED') {
-        isSessionOpen = false;
-        console.log('Sesión cerrada.');
-      }
-    });
   });
 
 app.post('/send-message', (req, res) => {
@@ -46,16 +36,57 @@ app.post('/send-message', (req, res) => {
 
     client.sendText(destinationNumber + '@c.us', message)
       .then((result) => {
-        console.log('Mensaje enviado: ', result);
+        // console.log('Mensaje enviado: ', result);
         res.status(200).send('Mensaje enviado con éxito');
       })
       .catch((error) => {
-        console.error('Error al enviar mensaje: ', error);
-        res.status(500).send('Error al enviar mensaje');
-    });
+        /* 
+          Añadir catch en la siguiente función: /node_modules/venom-bot/dist/api/layers/sender.layer.js::327
+          const result = await this.page.evaluate(({ to, content, passId, checkNumber, forcingReturn, delSend }) => {
+                return WAPI.sendMessage(to, content, undefined, passId, checkNumber, forcingReturn, delSend);
+            }, { to, content, passId, checkNumber, forcingReturn, delSend }).catch((error) => {
+                reject(error);
+            });
+        */
+
+        // console.log('Error al enviar mensaje: ');
+        // console.log(error);
+        res.status(500).send('Error al enviar mensaje. Posible causa: Desconexión de Whastapp.');
+      });
   }
 });
 
+app.post('/create-qr', (req, res) => {
+  client.close()
+    .then(() => {
+      isSessionOpen = false;
+
+      venom
+      .create({
+        session: 'session-name',
+        catchQR: (base64Qr, asciiQR) => {
+          // Cuando se necesita el escaneo del código QR, puedes enviarlo al cliente
+          // base64Qr contiene el código QR en formato base64
+          // asciiQR contiene el código QR en formato ASCII
+          console.log('Escanea este código QR:');
+          console.log(asciiQR);
+          res.status(200).send(asciiQR);
+        },
+        // statusFind: (statusGet, session, info) => {
+        //   console.log("Cambio en la sesión")
+        //   console.log(statusGet)
+        // }
+      })
+      .then(async (cl) => {
+        client = cl;
+        isSessionOpen = true;
+      });
+    })
+    .catch((error) => {
+      console.error('Error al cerrar la sesión:', error);
+    });
+
+});
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
